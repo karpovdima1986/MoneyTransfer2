7512e96ccfa187f36ef4d5c527d81492bf3f0fc5
Karpov Dima AN-43 <karpov.dima1986@mail.ru>
2024-03-04 19:35:15 +0300

    Correction

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,27 +1,30 @@
 package ru.netology
 
 import kotlin.math.max
+
 const val ERROR_TYPE = -1
 const val ERROR_LIMIT = -2
+fun main() {
+    val typeCard = "Мир"
+    val transfer = 80_000
+    val previous = 0
 
-
-fun main () {
-    println(comission("Visa", 10_000, 0))
-    println(comission("Мир", 15_000, 0))
-    println(comission("Mastercard", 150_000, 0))
-
-}
-fun comission(typeCard : String, transfer: Int, previous:Int): Int = when (typeCard){
-    "Visa" -> if (transfer < 150_000 && transfer + previous <= 600_000) {
-        max(35, (transfer * 0.0075).toInt())
-    }else {
-        ERROR_LIMIT
+    val comission = when (typeCard) {
+        "Visa" -> max(35, (transfer * 0.0075).toInt())
+        "Mastercard" -> if (previous > 75_000) {
+            (transfer * 0.006 + 20).toInt()
+        } else if (transfer + previous < 75_000) {
+            0
+        } else {
+            ((previous + transfer - 75_000) * 0.006 + 20).toInt()
+        }
+        "Мир" -> 0
+        else -> ERROR_TYPE
     }
-    "Mastercard" -> if (300 <= transfer + previous && transfer + previous <= 75_000) {
-        0
-    }else {
-        ((transfer - 75_000) * 0.006 + 20).toInt()
-    }
-    "Мир" -> 0
-    else -> ERROR_TYPE
+    val condition = if (transfer < 150_000 && transfer + previous <= 600_000) comission else ERROR_LIMIT
+
+    println(condition)
 }
+
+
+
