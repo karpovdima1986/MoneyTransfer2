40f48eefe1903d69e9d6b6af6063cf1dedeec0bf
Karpov Dima AN-43 <karpov.dima1986@mail.ru>
2024-03-06 20:31:14 +0300

    Correction2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,11 +5,21 @@ import kotlin.math.max
 const val ERROR_TYPE = -1
 const val ERROR_LIMIT = -2
 fun main() {
-    val typeCard = "Мир"
-    val transfer = 80_000
-    val previous = 0
+    println("Комиссия " +  comission(50_000, "Visa") + " рублей")
+}
 
-    val comission = when (typeCard) {
+//fun condition(typeCard : String,transfer: Int, previous:Int ) {
+//    if (transfer < 150_000 && transfer + previous <= 600_000)
+//        comission(transfer,typeCard, previous)  else ERROR_LIMIT
+//}
+
+fun comission(transfer: Int ,typeCard : String = "Мир" , previous:Int = 0 ): Int {
+    
+//    return (typeCard)  if (transfer < 150_000 && transfer + previous <= 600_000)
+//        comission(typeCard, transfer, previous)  else ERROR_LIMIT
+
+
+     return when (typeCard) {
         "Visa" -> max(35, (transfer * 0.0075).toInt())
         "Mastercard" -> if (previous > 75_000) {
             (transfer * 0.006 + 20).toInt()
@@ -18,13 +28,15 @@ fun main() {
         } else {
             ((previous + transfer - 75_000) * 0.006 + 20).toInt()
         }
+
         "Мир" -> 0
         else -> ERROR_TYPE
-    }
-    val condition = if (transfer < 150_000 && transfer + previous <= 600_000) comission else ERROR_LIMIT
 
-    println(condition)
-}
+    }
+    }
+
+
+
 
 
 
